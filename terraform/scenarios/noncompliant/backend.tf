54c18ebd03159a21bb35c3457fcd01c22eca77a2
ahesketh-sec <78739846+ahesketh-sec@users.noreply.github.com>
2026-01-05 16:19:28 +0000

    modified:   .gitignore new file:   terraform/scenarios/compliant/backend.tf new file:   terraform/scenarios/compliant/main.tf new file:   terraform/scenarios/noncompliant/backend.tf new file:   terraform/scenarios/noncompliant/main.tf

---
+++ terraform/scenarios/noncompliant/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "526053389648-terraform-state-bucket"
+    key            = "lab/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-state"
+    encrypt        = true
+    
+  }
+}
