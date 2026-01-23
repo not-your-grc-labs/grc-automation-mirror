859d3ced2cd3a3e5e2680dc006ccc82dcc2afccd
Andrew Hesketh <andrew.c.hesketh@gmail.com>
2026-01-23 16:05:46 +0000

    Update to S3 website

--- terraform/scenarios/noncompliant/s3.tf
+++ terraform/scenarios/noncompliant/s3.tf
@@ -63,8 +63,10 @@ resource "aws_s3_object" "index_html" {
 <html>
   <head><title>Public Demo</title></head>
   <body>
-    <h1>My public S3 website</h1>
+    <h1>I Love Coffee! :-) </h1>
+    <img src="coffee.png" alt="Logo" />
     <p>Terraform-managed content</p>
+
   </body>
 </html>
 EOF
